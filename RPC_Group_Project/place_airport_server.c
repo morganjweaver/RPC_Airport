1182651822acfa96916e5e162a3c8b6ac17cd01e
Morgan Weaver <morganjweaver@gmail.com>
2017-02-16 17:39:32 -0800

    server fix

--- RPC_Group_Project/place_airport_server.c
+++ RPC_Group_Project/place_airport_server.c
@@ -93,6 +93,9 @@ airport_lookup_1_svc(lat_long_input *argp, struct svc_req *rqstp)
 	float longitude = argp->longitude;
   errno = 0;
   static airport_ret  result;
+  int counter = 0;
+  float farr[2]; //holds kdtree values 
+  char *pch;
 
   if (!kdpop){
    populate_tree();
@@ -101,22 +104,46 @@ airport_lookup_1_svc(lat_long_input *argp, struct svc_req *rqstp)
  xdr_free((xdrproc_t)xdr_airport_ret, (char*)&result); 
  struct kdres *presults;
  const float pt[] = {lat, longitude};
- int search_range = 1;
+ int search_range = 0;
  presults = kd_nearest_rangef( kdtree, pt, search_range);
  airport_node* tail = malloc(sizeof(airport_node));
  airport_node* curr = tail;
  tail->next = NULL;
  curr = result.airport_ret_u.list;
  printf( "found %d results:\n", kd_res_size(presults) );
+ 
+ if(kd_res_size(presults) ==1){
+  counter++;
+  pch = (char*)kd_res_itemf( presults, farr );
+  double dist = distance((double)pt[0], (double)pt[1], (double)farr[0], (double)farr[1], 'M');
+  printf( "Node at (%.3f, %.3f) is %.3f away and has data=%s\n", 
+  farr[0], farr[1], dist, pch );
+  airport_node* temp = malloc(sizeof(airport_node));
+  temp->latitude = farr[0];
+  temp->longitude = farr[1];
+  char* output = (char*)calloc(15,1);
+  snprintf(output, 15, "%f", dist);
+  temp->distance = output;
+  temp->code = (char*)calloc(6,1);
+  strcpy(temp->code, pch);
+  temp->name = (char*)calloc(64,1);
+  strncpy(temp->name, pch+6, 63);
+  int i = 0;
+     while(isalpha(temp->name[i]) || ispunct(temp->name[i])) {
+      i++;
+    }
+    temp->name[i] = '\0';
+    temp->next = curr;
+    curr = temp;
+      //print out the retrieved data 
+    counter++;
+    kd_res_next( presults );
+ }
 
  while ((kd_res_size(presults) < 5)){
-   search_range *= 2;
+   search_range += 1;
    presults = kd_nearest_rangef( kdtree, pt, search_range);
  }
-    float farr[2]; //holds kdtree values 
-    int counter = 0;
-    char *pch;
-
     while( counter <5 && !kd_res_end( presults )) {
 	    // get the data and position of the current result item 
      pch = (char*)kd_res_itemf( presults, farr );
